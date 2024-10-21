79e433dc4cffafc9bc2a86744cfc41c97d95dce4
hochan2487 <hochan24877215@gmail.com>
2024-10-21 15:19:00 +0900

    Add files via upload

--- snake.py
+++ snake.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random
 
 def message(fonts, msg, color, posx, posy):
      mesg = fonts.render(msg, True, color)
@@ -8,14 +9,32 @@ def message(fonts, msg, color, posx, posy):
      mesg_Rect.centery = posy
      screen.blit(mesg, mesg_Rect)
 
+def food ():
+    global foodx, foody
+    while True:
+        foodx = random.randrange(10, DISPLAY_SIZE[0] - SNAKE_SIZE, SNAKE_SIZE)
+        foody = random.randrange(10, DISPLAY_SIZE[1] - SNAKE_SIZE, SNAKE_SIZE)
+        food_pos = [foodx, foody]
+
+        if food_pos in [snake_pos_x, snake_pos_y] :
+            continue
+        else :
+            break
 
 DISPLAY_SIZE = (800,600)
 #snake
 SNAKE_SIZE = 20
+snake_spped = 5
 snake_pos_x = DISPLAY_SIZE[0]/2 - SNAKE_SIZE/2
 snake_pos_y = DISPLAY_SIZE[1]/2 - SNAKE_SIZE/2
 snake_pos_x_change = 0
 snake_pos_y_change = 0
+
+#food
+foodx = None
+foody = None
+score = 0
+
 #color
 RED = (255,0,0)
 GREEN = (0,255,0)
@@ -24,6 +43,7 @@ WHITE= (255,255,255)
 GRAY= (127,127,127)
 
 pygame.init()
+
 #fonts
 font_gameOver = pygame.font.SysFont(None, 50)
 font_madeBy = pygame.font.SysFont(None, 20)
@@ -35,6 +55,8 @@ pygame.display.set_caption("SNAKE GAME ver 0.1")
 pygame.display.update()
 
 running = True
+food()
+
 while running:
     for event in pygame.event.get():
         #print(event)
@@ -43,32 +65,37 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 snake_pos_x_change = 0
-                snake_pos_y_change = -10
+                snake_pos_y_change = -20
             elif event.key == pygame.K_DOWN:
                   snake_pos_x_change = 0
-                  snake_pos_y_change = 10
+                  snake_pos_y_change = 20
             elif event.key == pygame.K_LEFT:
-                  snake_pos_x_change = -10
+                  snake_pos_x_change = -20
                   snake_pos_y_change = 0
             elif event.key == pygame.K_RIGHT:
-                  snake_pos_x_change = 10
+                  snake_pos_x_change = 20
                   snake_pos_y_change = 0
                   
                   
     snake_pos_x += snake_pos_x_change
     snake_pos_y += snake_pos_y_change
     screen.fill(WHITE)
+    if foodx == snake_pos_x and foody == snake_pos_y:
+        food()
+        score += 10
+        
+    pygame.draw.rect(screen, BLUE, [foodx, foody, 20, 20])
     pygame.draw.rect(screen,GRAY, [0,0, DISPLAY_SIZE[0], DISPLAY_SIZE[1]],10)
     pygame.draw.rect (screen, RED, [snake_pos_x, snake_pos_y, \
                                                              SNAKE_SIZE, SNAKE_SIZE])
-    if snake_pos_x >= (DISPLAY_SIZE[0] - SNAKE_SIZE - 10) or \
-       snake_pos_x - (SNAKE_SIZE/2) - 10 < 0 or \
-       snake_pos_y >= (DISPLAY_SIZE[1] - SNAKE_SIZE - 10) or \
-       snake_pos_y - (SNAKE_SIZE/2) - 10 < 0 :
+    if snake_pos_x >= (DISPLAY_SIZE[0] - SNAKE_SIZE - 5) or \
+       snake_pos_x - (SNAKE_SIZE/2) - 5 < 0 or \
+       snake_pos_y >= (DISPLAY_SIZE[1] - SNAKE_SIZE - 5) or \
+       snake_pos_y - (SNAKE_SIZE/2) - 5 < 0 :
         running = False
         
     pygame.display.update()
-    clock.tick(30)
+    clock.tick(19)
 
 message (font_gameOver, 'Game Over', RED, int(DISPLAY_SIZE[0]/2), int(DISPLAY_SIZE[1]/2))
 message (font_gameOver, 'user', GRAY, int(DISPLAY_SIZE[0]/2), int(DISPLAY_SIZE[1]/2) + 30)
